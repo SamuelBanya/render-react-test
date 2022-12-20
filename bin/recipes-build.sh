56de3f0bbbfe83f75964b35cd28cd2516bbc14f1
Samuel Banya <sbanya@fastmail.com>
2022-12-20 10:59:11 -0600

    Adding latest changes

--- bin/recipes-build.sh
+++ bin/recipes-build.sh
@@ -2,6 +2,14 @@
 # exit on error
 set -o errexit
 
+# NOTE:
+# Adding build commands for frontend for Render.com
+rm -rf pulic
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
 bundle install
 bundle exec rake db:migrate 
-bundle exec rake db:seed
+# NOTE:
+# Commenting out seed data since we already seeded the database locally:
+# bundle exec rake db:seed
